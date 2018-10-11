c3aee3ac61d33f527a7aaf117716290d9a055327
luisibarra06 <42873966+luisibarra06@users.noreply.github.com>
2018-10-11 10:26:11 -0700

    Add files via upload

---
+++ htaccess-rename.sh
@@ -0,0 +1,8 @@
+mv htaccess  .htaccess;
+mv h.html.txt .h.html;
+mv f.html.txt .f.html;
+# do not run this as root
+# as a not root user type
+# $chmod a+x htaccess-rename.sh
+# run
+# ./htaccess-rename.sh
